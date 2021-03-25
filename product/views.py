b23253a44925f91e787aa1ba1398d58d5ceaa310
vika728 <vika.sokolova728@gmail.com>
2021-03-25 18:42:21 +0600

    first commit

---
+++ product/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from product.models import Category, Product
+
+
+def home_page(request):
+    categories = Category.objects.all()
+    return render(request, 'product/home.html', {'categories': categories})
+
+
+def product_list(request, slug):
+    products = Product.objects.filter(category__slug=slug)
+    return render(request, 'product/list.html', locals())
+def product_detail(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return  render(request, 'product/detail.html', {'product': product})
