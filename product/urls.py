66fe26368ff94c965e5fb3f492aec3a04656e290
git@github.com:vika728/djangogirls.git <vika728>
2021-03-29 17:41:29 +0600

    new

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path('', home_page, name='home'),
     path('<str:slug>/', product_list, name='list'),
     path('product/<int:product_id>/', product_detail, name='detail'),
+    path('product/create/', create_product, name='create-product'),
+    path('product/update/<int:product_id>/', update_product, name='update-product'),
+    path('product/delete/<int:product_id>/', delete_product, name="delete-product"),
 ]
